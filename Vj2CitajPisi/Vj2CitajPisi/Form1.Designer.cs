a267291bef2aae3b07535d234ed9fce8cfe47cc6
IvicaFister1 <51374233+IvicaFister1@users.noreply.github.com>
2019-06-05 07:37:19 +0200

    Add files via upload

---
+++ Vj2CitajPisi/Vj2CitajPisi/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Vj2CitajPisi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Ucitaj_iz = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.upisPutanje1 = new System.Windows.Forms.TextBox();
+            this.poljeIspisa = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.poljeUpisa = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.upisPutanje2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.imeDokumenta = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
+            // 
+            // Ucitaj_iz
+            // 
+            this.Ucitaj_iz.Location = new System.Drawing.Point(12, 42);
+            this.Ucitaj_iz.Name = "Ucitaj_iz";
+            this.Ucitaj_iz.Size = new System.Drawing.Size(101, 23);
+            this.Ucitaj_iz.TabIndex = 1;
+            this.Ucitaj_iz.Text = "Otvori datoteku";
+            this.Ucitaj_iz.UseVisualStyleBackColor = true;
+            this.Ucitaj_iz.Click += new System.EventHandler(this.Ucitaj_iz_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(119, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Putanja datoteke:";
+            // 
+            // upisPutanje1
+            // 
+            this.upisPutanje1.Location = new System.Drawing.Point(213, 45);
+            this.upisPutanje1.Name = "upisPutanje1";
+            this.upisPutanje1.Size = new System.Drawing.Size(498, 20);
+            this.upisPutanje1.TabIndex = 3;
+            // 
+            // poljeIspisa
+            // 
+            this.poljeIspisa.Location = new System.Drawing.Point(122, 103);
+            this.poljeIspisa.Multiline = true;
+            this.poljeIspisa.Name = "poljeIspisa";
+            this.poljeIspisa.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.poljeIspisa.Size = new System.Drawing.Size(589, 74);
+            this.poljeIspisa.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(119, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(169, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Ispis sadržaja odabrane datoteke: ";
+            // 
+            // poljeUpisa
+            // 
+            this.poljeUpisa.Location = new System.Drawing.Point(122, 218);
+            this.poljeUpisa.Multiline = true;
+            this.poljeUpisa.Name = "poljeUpisa";
+            this.poljeUpisa.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.poljeUpisa.Size = new System.Drawing.Size(589, 74);
+            this.poljeUpisa.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(119, 202);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Upis teksta u datoteku: ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 324);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Spremi u: ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(119, 329);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Putanja datoteke: ";
+            // 
+            // upisPutanje2
+            // 
+            this.upisPutanje2.Location = new System.Drawing.Point(213, 327);
+            this.upisPutanje2.Name = "upisPutanje2";
+            this.upisPutanje2.Size = new System.Drawing.Size(498, 20);
+            this.upisPutanje2.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(121, 359);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(86, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Ime dokumenta: ";
+            // 
+            // imeDokumenta
+            // 
+            this.imeDokumenta.Location = new System.Drawing.Point(213, 356);
+            this.imeDokumenta.Name = "imeDokumenta";
+            this.imeDokumenta.Size = new System.Drawing.Size(498, 20);
+            this.imeDokumenta.TabIndex = 12;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(636, 403);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "SPREMI";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.imeDokumenta);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.upisPutanje2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.poljeUpisa);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.poljeIspisa);
+            this.Controls.Add(this.upisPutanje1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Ucitaj_iz);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Moja forma";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Ucitaj_iz;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox upisPutanje1;
+        private System.Windows.Forms.TextBox poljeIspisa;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox poljeUpisa;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox upisPutanje2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox imeDokumenta;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Vj2CitajPisi/Vj2CitajPisi/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vj2CitajPisi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Ucitaj_iz_Click(object sender, EventArgs e)
+        {
+            
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //otaranje File Dialoga i odabir datoteke
+                string putanja1 = openFileDialog1.FileName;
+                upisPutanje1.Text = putanja1;
+
+                string readText = File.ReadAllText(putanja1);
+                poljeIspisa.Text = readText;
+            }
+            else
+            {
+                upisPutanje1.Text = "Datoteka nije odabrana";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog Folder = new FolderBrowserDialog();
+
+            if (Folder.ShowDialog() == DialogResult.OK)
+            {
+                //otvaranje File Dialoga i odabir datoteke
+                string putanja2 = Folder.SelectedPath;
+                upisPutanje2.Text = putanja2;
+
+              
+               
+            }
+            else
+            {
+                upisPutanje2.Text = "";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string putanjaSadrzaj;
+            string imeDokSadrzaj;
+           
+            string path;
+
+            putanjaSadrzaj = upisPutanje2.Text;
+            imeDokSadrzaj = imeDokumenta.Text;
+            
+            path = putanjaSadrzaj+@"\"+imeDokSadrzaj+@".txt";
+
+
+            try
+            {
+                if ((string.IsNullOrEmpty(imeDokSadrzaj)||imeDokSadrzaj.Trim().Length==0)&&(string.IsNullOrEmpty(putanjaSadrzaj)||putanjaSadrzaj.Trim().Length==0))
+                    //(imeDokSadrzaj.Length == 0 && putanjaSadrzaj.Length == 0)
+                {
+                    MessageBox.Show("Ime dokumenta i putanja datoteke nisu upisani!");
+                }
+                else if (string.IsNullOrEmpty(imeDokSadrzaj) || imeDokSadrzaj.Trim().Length == 0)
+                //(imeDokSadrzaj.Length == 0)
+
+                {
+                    MessageBox.Show("Ime dokumenta nije upisano!");
+                }
+                else if (string.IsNullOrEmpty(putanjaSadrzaj) || putanjaSadrzaj.Trim().Length == 0)
+                //(putanjaSadrzaj.Length == 0)
+                {
+                    MessageBox.Show("Putanja datoteke nije upisana!");
+                }
+                else
+                {
+
+                    {
+                        using (StreamWriter sw = new StreamWriter(path, true))
+                        {
+
+                            sw.WriteLine(poljeUpisa.Text);
+                            MessageBox.Show("Uspješno upisano u datoteku: " + imeDokSadrzaj + @".txt");
+
+
+                        }
+
+                    }
+                }
+             
+
+            }
+            catch(DirectoryNotFoundException)
+            {
+                MessageBox.Show("Polje unosa putanje datoteke nije upisano!");
+            }
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show("Polje unosa putanje datoteke ili imena dokumenta nije upisano!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Greška: {0}",ex.Message);
+            }
+
+        }
+            
+
+
+
+
+    }
+}
